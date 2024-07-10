3a514c230f4de108fa4d9a7044363ecf2d40c253
PrincePandeycode <pandeyprince4723@gmail.com>
2024-07-10 23:51:40 +0530

    datatype conversiosn

---
+++ 3.js
@@ -0,0 +1,40 @@
+// num = 54
+// console.log( typeof num) // number
+
+// let strNUm = String(num)
+// console.log(typeof strNUm) // String
+
+// let BoolNum = Boolean(num)
+// console.log(BoolNum)
+// console.log(typeof BoolNum)
+
+// let str = '123';
+// let num1 = Number(str); // Using Number constructor
+// let num2 = parseInt(str); // Using parseInt for integer
+// let num3 = parseFloat(str); // Using parseFloat for floating-point number
+// console.table(typeof num1)
+// console.table(typeof num2)
+// console.table(typeof num3)
+
+// nan
+// str = "23df"
+// num5 = Number(str)
+// console.log(num5)
+
+// let str = 'hello';
+// let bool = Boolean(str); // Non-empty string becomes true
+// console.log(bool)
+
+// let emptyStr = '';
+// let boolEmpty = Boolean(emptyStr); // Empty string becomes false
+// console.log(boolEmpty)
+
+let score = null
+
+console.log(score)
+
+let scr = Number(score)  // null becomes 0 if convert to string
+
+console.log(scr)
+
+
