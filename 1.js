0094e74b5583635dd11962115aa94a302d10c825
PrincePandeycode <pandeyprince4723@gmail.com>
2024-07-10 16:00:56 +0530

    let,const,var

--- 1.js
+++ 1.js
@@ -1,1 +1,14 @@
 console.log("hello there")
+const accountId= 234
+var accName= "prince"
+let accmail = "abc@mail.com"
+price = 45;
+
+// accountId = 45   --> throws : TypeError: Assignment to constant variable.
+// console.log(accountId)
+
+accName = true          // we can change the datatpe while reassigning valus here
+accmail = 78
+price = 99
+
+console.table([accName,accmail,price])
