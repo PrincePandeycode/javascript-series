4e26a70f3276990365df1f16509a9f91f094bb09
PrincePandeycode <pandeyprince4723@gmail.com>
2024-07-12 00:19:19 +0530

    strings

---
+++ 6strings.js
@@ -0,0 +1,29 @@
+ const str = "Prince"
+ const str2 = "Pandey"
+
+ console.log(str+" "+str2);
+
+//  but mostly we use bactics
+
+console.log(`the name is ${str} and the surname is ${str2}`)
+
+// or
+ const name = new String("  Prince pandey is a good boy")
+ console.log(name)
+
+ console.log(name.length)
+ console.log(name.toUpperCase())
+ console.log(name.toLowerCase())
+ console.log(name.charAt(3))
+ console.log(name.indexOf('e'))
+
+ console.log(name.includes('is'))
+ console.log(name.trim())
+ console.log(name.replace('i','s'))
+
+console.log(name.split(" "))
+
+// after a sw update keyboard is working again
+
+
+
