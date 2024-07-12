07509921ff4a17aeb5f6ba51ccf7fafe42d95d14
PrincePandeycode <pandeyprince4723@gmail.com>
2024-07-12 15:14:45 +0530

    maths and Numbers

---
+++ 7_mathsNumbers.js
@@ -0,0 +1,31 @@
+// const score= 400;
+// const balance = new Number(500)
+
+// console.log(score)
+// console.log(balance)
+
+// console.log(num.toLocaleString())
+// console.log(Number.MIN_VALUE)
+// console.log(Number.MAX_VALUE)
+
+// const num= 1000000
+
+// console.log(num.toString().length)
+
+// some maths
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.floor(4.2))
+// console.log(Math.ceil(4.2))
+// console.log(Math.random()) //betwnn 0 and 1
+
+// console.log(Math.random()*10+1) //between 1 and 10
+// console.log(Math.floor(Math.random()*10+1)) //between 1 and 10
+
+// betwen an Range
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() *(max-min+1)+min))
