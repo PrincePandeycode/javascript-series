59729581e097cc9c23ca9df84bba623a23b91e3d
PrincePandeycode <pandeyprince4723@gmail.com>
2024-08-06 22:07:29 +0530

    apply and bind in js

--- 31_call.js
+++ 31_call.js
@@ -30,4 +30,19 @@ const person = {
 
 console.log(FullName.printit.call(person))
 
+// 3.the primary difference between apply and call in JavaScript lies in how arguments are passed to the function:
+
+function sum(a, b, c) {
+    return a + b + c;
+}
+
+const numbers = [1, 2, 3];
+
+// Using apply
+console.log(sum.apply(null, numbers)); // Output: 6
+
+// Using call
+console.log(sum.call(null, 1, 2, 3)); // Output: 6
+
+
 
